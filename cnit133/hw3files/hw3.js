b43adb0ba47ca4bf7b31254a30de68803e3ec915
justinino <46943368+justinino@users.noreply.github.com>
2019-02-22 15:15:54 -0800

    Update hw3.js

--- cnit133/hw3files/hw3.js
+++ cnit133/hw3files/hw3.js
@@ -28,7 +28,7 @@ function process() {
             grade = 'F'
             comment = 'Student must retake course'
         }
-        document.getElementById('finalgrade').innerHTML = "Final Average: " + final_average + "\n" + "Grade: " + grade + "\n" + comment;
+        document.getElementById('finalgrade').innerHTML = "Final Average: " + final_average + "<br>" + "Grade: " + grade + "<br>" + comment;
     }
 }
 
