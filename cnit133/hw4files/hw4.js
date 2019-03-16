456861706e72cc3133f05272031a8b973f154c6d
justinino <46943368+justinino@users.noreply.github.com>
2019-03-15 23:53:09 -0700

    Update hw4.js

--- cnit133/hw4files/hw4.js
+++ cnit133/hw4files/hw4.js
@@ -1,1 +1,10 @@
-
+/* Part 2 */
+function addition() {
+    var temp1, temp2, temp3, total, salary, i, j;
+    text = '';
+    result = 0;
+    for (i = 5; i < 21; i = i + 3) {
+      result = result + i
+    }
+     document.getElementById('3plusresult').innerHTML = ''
+}
