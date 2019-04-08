5c93c7f2abb8547f72100373f795180e9d3caf96
justinino <46943368+justinino@users.noreply.github.com>
2019-04-07 23:29:58 -0700

    Update hw5.js

--- cnit133/hw5files/hw5.js
+++ cnit133/hw5files/hw5.js
@@ -1,1 +1,32 @@
-
+function validateForm() {
+    var x = myForm.name.value;
+    var y = myForm.animal.value;
+    var z = myForm.gender.options[myForm.gender.selectedIndex].value;
+    var q = myForm.colour;
+    var checkedValue = null; 
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+    if (y == "") {
+        alert("Animal type must be picked");
+        return false;
+    }    
+    if (z == "") {
+        alert("Drop down list must be selected");
+        return false;
+    }
+    for (var i=0; q[i]; i++){
+        if(q[i].checked){
+             checkedValue = q[i].value;
+        }
+    }
+    if (checkedValue == null) {
+        alert("At least one select box must be checked");
+        return false;
+    }
+  }
+function urlGo(myForm) {
+    var mydest = myForm.destList.options[myForm.destList.selectedIndex].value;
+    window.location = mydest;
+}
